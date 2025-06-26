b20e3eca449de0a0b42b251db07356a5709c141c
declan_dev <declannashold@gmail.com>
2025-06-26 00:41:04 -0500

    Front end fixes and tweaks

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -9,4 +9,41 @@ module ApiHelper
             "danger"
         end
    end
+
+   def windMPH(wind)
+        case wind
+        when ..10
+            "primary"
+        when 11..25
+            "warning"
+        else
+            "danger"
+        end
+   end
+
+    def gustMPH(gust)
+        case gust
+        when ..15
+            "primary"
+        when 16..30
+            "warning"
+        else
+            "danger"
+        end
+   end
+
+    def windchillTemp(temp)
+        case temp
+        when ..-20
+            "primary"
+        when -20..32
+            "info"
+        when 0..32
+            "secondary"
+        else
+            "warning"
+        end
+   end
+
+
 end
